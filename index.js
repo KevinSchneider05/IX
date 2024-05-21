fae8b4af4a3aac2873a3f6330bb804365db88547
KevinSchneider05 <kevins05@vt.edu>
2024-05-21 22:47:48 +0200

    v1

---
+++ index.js
@@ -0,0 +1,12 @@
+let fibSequence = prompt("How many integers in the sequence?")
+
+let n1 = 0;
+let n2 = 1;
+
+for (let i = 1; i <= fibSequence; i++) {
+console.log(n1);
+    n3  = n1 + n2;
+    n1 = n2;
+    n2 = n3
+}
+console.log(fib)
